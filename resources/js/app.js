65d0e317143a42d56c9467b346c4940c874fef43
Yousaf Arshad <yousaf.bsse1735@gmail.com>
2021-06-02 09:11:38 +0500

    vue installation and configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,14 @@
 require('./bootstrap');
 
 require('alpinejs');
+
+import Vue from 'vue/dist/vue.js';
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+var app = new Vue({
+    el: '#app',
+    methods: {
+        open () {}
+    },
+});
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="border-4 border-green-800 border-double rounded-lg px-5 py-3 shadow-lg bg-yellow-100 m-auto w-40 mt-12">Hello World.</h1>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      console.log("Example component mounted");
+    }
+  };
+</script>
